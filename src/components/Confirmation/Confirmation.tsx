e23a500df32c97451835cc3a04e06d8b564a5fba
tobaojo <toba_ojo@hotmail.com>
2025-02-28 13:22:03 +0000

    updated spelling

--- src/components/Confirmation/Confirmation.tsx
+++ src/components/Confirmation/Confirmation.tsx
@@ -5,11 +5,13 @@ const Confirmation = () => {
   return (
     <div className="flex flex-col space-y-6 mx-auto align-middle">
       <Image src={icon} alt="thank-you" className="self-center mt-4" />
-      <h1 className="font-bold text-marineBlue text-[1.8rem] justify-center text-center">Thank you!</h1>
+      <h1 className="font-bold text-marineBlue text-[1.8rem] justify-center text-center">
+        Thank you!
+      </h1>
       <p className="text-coolGray justify-center text-center text-[1.2rem]">
-        Thanks for confirming your prescription!<br></br> We hope you have fun using pur
-        platform. If you ever need support, please feel free to email us at
-        support@loremgaming.com
+        Thanks for confirming your prescription!<br></br> We hope you have fun
+        using our platform. If you ever need support, please feel free to email
+        us at support@loremgaming.com
       </p>
     </div>
   );
